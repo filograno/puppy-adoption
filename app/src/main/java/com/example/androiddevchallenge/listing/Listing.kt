e392809488df11e1fe67badcd7665d4747f5523f
Giuseppe Filograno <filograno@gmail.com>
2021-03-02 19:45:22 +0100

    Use LazyColumn to display the list

--- app/src/main/java/com/example/androiddevchallenge/listing/Listing.kt
+++ app/src/main/java/com/example/androiddevchallenge/listing/Listing.kt
@@ -4,17 +4,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.model.Puppy
 import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.example.androiddevchallenge.ui.theme.typography
@@ -23,12 +27,12 @@ import com.example.androiddevchallenge.ui.theme.typography
 fun MyApp(puppies: List<Puppy>) {
     MaterialTheme {
         Surface(color = MaterialTheme.colors.background) {
-            Column(
+            LazyColumn(
                 Modifier
                     .padding(16.dp)
                     .fillMaxWidth()
             ) {
-                puppies.forEach { puppy ->
+                items(puppies) { puppy ->
                     PuppyItem(puppy)
                     Spacer(modifier = Modifier.height(8.dp))
                 }
